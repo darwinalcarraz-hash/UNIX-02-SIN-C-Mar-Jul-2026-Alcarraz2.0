11062f6f73ccb8a567a47fa0df675940d38b8e56
darwinalcarraz-hash <darwinalcarraz@gmail.com>
2026-03-30 15:38:11 +0000

    Bios

--- Fs_exploration.sh
+++ Fs_exploration.sh
@@ -1,6 +1,18 @@
-#this com
+#this command does
 sudo apt update
 #
 sudo apt upgrade
 #
 sudo apt install parted
+#
+pip install numpy
+#
+sudo parted -l && echo -e "\n---\n" && lsb.
+#
+sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n"
+#
+sudo parted -l
+#
+lsblk -f
+#
+[ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS"
