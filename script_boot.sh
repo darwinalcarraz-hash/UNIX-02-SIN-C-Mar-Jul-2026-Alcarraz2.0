121c885333639193580fc6b4bddcf99f2803f434
darwinalcarraz-hash <darwinalcarraz@gmail.com>
2026-04-08 21:19:25 +0000

    wip: list all inodo

--- script_boot.sh
+++ script_boot.sh
@@ -1,4 +1,8 @@
 #Relative path
 cd caluculadora 
 #Absolute route
-/workspaces/UNIX-02-SIN-C-Mar-Jul-2026-Alcarraz2.0/Calculadora #Absolute route
+/workspaces/UNIX-02-SIN-C-Mar-Jul-2026-Alcarraz2.0/Calculadora 
+#l=list a=all i=inodo
+ls -lai
+#the same option but in a separate way
+ls -l -a -i
