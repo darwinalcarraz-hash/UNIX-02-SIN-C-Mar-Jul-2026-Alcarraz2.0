fb613793c0ec61bf8401f45351c0c0235a85b51c
darwinalcarraz-hash <darwinalcarraz@gmail.com>
2026-04-08 21:36:07 +0000

    wip: stat . command

--- script_boot.sh
+++ script_boot.sh
@@ -6,3 +6,13 @@ cd caluculadora
 ls -lai
 #the same option but in a separate way
 ls -l -a -i
+#The "stat ."" command displays information about the current directory, such as device 7,4, indicating that we are using a physical disk.
+stat .
+#File: .
+#Size: 4096            Blocks: 8          IO Block: 4096   directory
+#Device: 7,4     Inode: 1573114     Links: 2
+#Access: (0777/drwxrwxrwx)  Uid: ( 1000/codespace)   Gid: ( 1000/codespace)
+#Access: 2026-04-08 21:08:28.893805905 +0000
+#Modify: 2026-04-08 21:08:26.810805986 +0000
+#Change: 2026-04-08 21:08:26.810805986 +0000
+#Birth: 2026-04-08 21:08:26.810805986 +0000
