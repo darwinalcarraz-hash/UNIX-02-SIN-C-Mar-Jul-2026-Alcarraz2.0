96e66f1982fb5d15f4e9185ea6ffe990f424e26e
darwinalcarraz-hash <darwinalcarraz@gmail.com>
2026-04-17 14:28:41 +0000

    wip: ls combined

--- Boot_exploration.sh
+++ Boot_exploration.sh
@@ -1,4 +1,10 @@
-#MShow the files in the folder
+#Show the files in the folder
 ls -a
 #Show the files in the folder
 ls --all
+#Individual flags
+ls -l -a -h  
+#Mixed flags
+ls -l -ah
+#Combined flags (The most common way to write it)    
+ls -lah  
