633508f90afffc66c489f4f0b904779e38fd244e
darwinalcarraz-hash <darwinalcarraz@gmail.com>
2026-04-30 00:48:22 +0000

    wip: "ls" command

---
+++ linux_cisco_1.sh
@@ -0,0 +1,7 @@
+#The "ls" command displays a list of files contained in the current directory.
+@darwinalcarraz-hash ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026-Alcarraz2.0 (linux_cisco_1) $ ls
+Calculadora                          da_llave_publica.asc    doc_no_cifrado.txt.sig        final_cifrado_firmado.txt.gpg
+Cayetano_llave_publica.asc           doc_cifrado.txt         doc_no_cifrado_CayeC.txt      gnupg_script.sh
+LICENSE                              doc_cifradoCAYE.txt     doc_no_cifrado_CayeC.txt.gpg  linux_cisco_1.sh
+README.md                            doc_no_cifrado.txt      doc_no_cifrado_CayeC.txt.sig  llave_privada_clase.asc
+cifrado_firmado_final_CayeC.txt.gpg  doc_no_cifrado.txt.gpg  doc_no_cifrado_firmado.txt    verificacion_Cayetano.txt
