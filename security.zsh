c0179890a9153c2142f75c6918140563857f3d6d
darwinalcarraz-hash <darwinalcarraz@gmail.com>
2026-05-08 15:32:59 +0000

    wip: Group management and GID range verification in login.defs

--- security.zsh
+++ security.zsh
@@ -17,4 +17,21 @@ id -u
 21  id -g
 #All groups ID
 22  id -G
-              
+#Creates a new group named "desarrolladores".
+groupadd desarrolladores
+#Creates a new group named "operaciones" with a specific Group ID (GID) of 2000.
+groupadd -g 2000 operaciones
+#Creates a new system group named "servicios_web" (usually with a lower GID).
+groupadd --system servicios_web
+#Displays the content of the file that defines the system groups.
+cat /etc/group
+#Searches and filters the group file to show only the three groups previously created.
+grep -E "desarrolladores|operaciones|servicios_web" /etc/group
+#Filters the login configuration file to see the range of GIDs available for normal and system users.
+[Analista-🐧Darwin-Alcarraz69🐧] UNIX-02-SIN-C-Mar-Jul-2026-Alcarraz2.0 ✘ 130 # grep -E "GID_MIN|GID_MAX|SYS_GID" /etc/login.defs
+GID_MIN                  1000
+GID_MAX                 60000
+#SYS_GID_MIN              101
+#SYS_GID_MAX              999
+SUB_GID_MIN                100000
+SUB_GID_MAX             600100000
