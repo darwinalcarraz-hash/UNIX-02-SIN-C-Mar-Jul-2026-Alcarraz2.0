306f676c28078e9689eb74c977590f35ea14b1a3
darwinalcarraz-hash <darwinalcarraz@gmail.com>
2026-05-29 15:36:12 +0000

    wip: install and configure newgrp via util-linux-extra

--- security2.zsh
+++ security2.zsh
@@ -20,3 +20,33 @@ ls -la ~/before_newgrp.txt
 #[Analista-🐧Darwin-Alcarraz69🐧] UNIX-02-SIN-C-Mar-Jul-2026-Alcarraz2.0 ✓ # ls -la ~/before_newgrp.txt
 #-rw-r--r-- 1 root root 0 may 29 15:05 /root/before_newgrp.txt
 
+# Updates the system's package index (list of available repositories)
+apt update
+
+# Upgrades all installed packages to their latest available version
+apt upgrade
+
+# Installs the 'util-linux-extra' package which provides the 'newgrp' command
+# (allows switching the active group in the current session without logging out)
+apt install util-linux-extra
+
+# Switches the active group of the current session to 'desarrolladores'
+# Opens a new subshell with that group set as the effective GID
+newgrp desarrolladores
+
+# Displays the name of the current effective group (verifies the switch was successful)
+id -gn
+
+# Displays the PID (Process ID) of the current shell
+# Useful to confirm that 'newgrp' spawned a new subshell (different PID than before)
+echo $$
+# Output: 20695  →  PID of the new subshell created by newgrp
+
+# Creates an empty file in the user's home directory to verify
+# that newly created files inherit 'desarrolladores' as the owning group
+touch ~/after_newgrp.txt
+
+# Lists the detailed attributes of the newly created file (permissions, owner, group, date)
+ls -la ~/after_newgrp.txt
+# Output: -rw-r--r-- 1 root desarrolladores 0 may 29 15:28 /root/after_newgrp.txt
+#         └─ confirms that the owning group is 'desarrolladores', not 'root'
