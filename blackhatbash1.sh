4f5b5fe368f8e2a86e4f636d170c4094f81c0812
darwinalcarraz-hash <darwinalcarraz@gmail.com>
2026-06-01 15:07:50 +0000

    wip: add bash trace and restricted execution commands

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+#!/usr/bin/env bash
 bash --version
 env
 echo ${SHELL}
@@ -5,3 +7,10 @@ echo ${SHELL}
 echo ${RANDOM}
 echo ${UID}
 echo ${OSTYPE}
+ps -ef
+df --human-readable
+
+# Debugs with extra chatter (verbose mode: prints every single step and variable value).
+bash -x blackhatbash1.sh
+# Runs in quiet lock-down (silent sandbox: restricts commands without telling you much).
+bash -r blackhatbash1.sh
