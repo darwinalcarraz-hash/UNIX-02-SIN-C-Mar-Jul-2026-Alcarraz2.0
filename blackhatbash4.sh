795871f234f3b40e157fbc99767bd7d8a3982b36
darwinalcarraz-hash <darwinalcarraz@gmail.com>
2026-06-05 14:34:12 +0000

    wip: Arithmetic Operators

---
+++ blackhatbash4.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+let result="4 * 5"
+echo ${result}
+20
+#-------------------
+result=$((5*5))
+echo ${result}
+25
+#-------------------
+result=$(expr 5 + 505)
+echo ${result}
+510
