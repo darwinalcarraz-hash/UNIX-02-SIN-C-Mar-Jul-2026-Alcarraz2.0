faa5131bcf9fcd4c86b2c09018a02ff511d313a7
darwinalcarraz-hash <darwinalcarraz@gmail.com>
2026-06-05 15:38:55 +0000

    wip: Redirection Operators

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -3,3 +3,18 @@ touch test && touch test123
 (ls; ps)
 ls; ps; whoami
 lzl || echo "El comando lzl fallo"
+#--------------------------------
+ echo "Hello World!" > output.txt
+cat output.txt
+echo "Goodbye!" >> output.txt
+cat output.txt
+#-------------------------------
+ls -l / &> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+#-------------------------------
+ls -l / &>> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+#-------------------------------
+ls -l / 1> stdout.txt 2> stderr.txt
+cat stdout.txt
+cat stderr.txt 
