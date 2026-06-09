0894a812a9c34594fb113df5258c9879845789a5
darwinalcarraz-hash <darwinalcarraz@gmail.com>
2026-06-09 23:34:14 +0000

    commit: if Conditions !

--- test_if_file_exists.sh
+++ test_if_file_exists.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-FILENAME="flow_control_with_if.txt"
-if [[ -f "${FILENAME}" ]]; then
-    echo "${FILENAME} already exists."
-    exit 1
-else
+FILENAME="flow_control_with_if2.txt"
+
+if [[ ! -f "${FILENAME}" ]]; then
     touch "${FILENAME}"
 fi
