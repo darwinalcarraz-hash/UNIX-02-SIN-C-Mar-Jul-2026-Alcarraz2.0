3105b8bd389332f27ec647102432961ce0f9a963
darwinalcarraz-hash <darwinalcarraz@gmail.com>
2026-06-19 15:32:25 +0000

    wip: Filtering with awk

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -18,3 +18,12 @@ ps | grep -i tty
 grep -v "35.237.4.214" log.txt
 #------------------------------------
 grep -o "35.237.4.214" log.txt
+#------------------------------------
+awk '{print $1}' log.txt
+awk '{print $3}' log.txt
+awk '{print $1,$NF}' log.txtawk '{print $1,$2,$3}' log.txt
+#------------------------------------
+touch test.csv
+echo -e "holaa1,chaoo1\nholaaa2,chaoo2\nholaa3,chaooo3" > test.csv
+cat test.csv
+#------------------------------------
